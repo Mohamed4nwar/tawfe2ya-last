e4a400f5cb1efa564e8984640869f97c750960a4
Anwar <116309098+Mohamed4nwar@users.noreply.github.com>
2025-08-31 15:20:43 +0300

    tawfe2ya-withbackend

---
+++ app/layout.js
@@ -0,0 +1,35 @@
+import { Outfit } from "next/font/google";
+import "./globals.css";
+import { AppContextProvider } from "@/context/AppContext";
+import { Toaster } from "react-hot-toast";
+import {
+  ClerkProvider,
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from '@clerk/nextjs'
+
+const outfit = Outfit({ subsets: ['latin'], weight: ["300", "400", "500"] })
+
+export const metadata = {
+  title: "tawfe2ya Store",
+  description: "E-Commerce with Next.js ",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <ClerkProvider>
+
+      <html lang="en">
+        <body className={`${outfit.className} antialiased text-gray-700`} >
+          <Toaster />
+          <AppContextProvider>
+            {children}
+          </AppContextProvider>
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
